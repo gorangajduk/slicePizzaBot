3079d7e28ff5fa775681bcb92460a319a2f9bd85
Goran Gajduk <goran.gajduk@rewardgateway.com>
2019-03-01 17:36:51 +0100

    added delivery validator

---
+++ pizzaBot/algorithm/Executor.swift
@@ -0,0 +1,8 @@
+//
+//  Executor.swift
+//  pizzaBot
+//
+//  Created by G Gajduk on 01/03/2019.
+//
+
+import Foundation
---
+++ pizzaBot/helper/DeliveryValidator.swift
@@ -0,0 +1,21 @@
+//
+//  DeliveryValidator.swift
+//  pizzaBot
+//
+//  Created by G Gajduk on 01/03/2019.
+//
+
+import Foundation
+
+class DeliveryValidator: NSObject {
+    class func validateDelivery(delivery: DeliveryOrder) -> Bool {
+        // iterate through all delivery points
+        for deliveryPoint in delivery.deliveryPoints {
+            // if a delivery point is invalid the delivery order is also considered invalid
+            if !delivery.map.isDeliveryPointValid(deliveryPoint: deliveryPoint) {
+                return false
+            }
+        }
+        return true
+    }
+}
