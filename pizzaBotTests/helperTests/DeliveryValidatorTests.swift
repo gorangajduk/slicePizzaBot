76a9479d743181d1a73653d0a28a516f9e40e453
Goran Gajduk <goran.gajduk@rewardgateway.com>
2019-03-01 20:20:53 +0100

    removed name

--- pizzaBotTests/helperTests/DeliveryValidatorTests.swift
+++ pizzaBotTests/helperTests/DeliveryValidatorTests.swift
@@ -2,7 +2,7 @@
 //  DeliveryValidatorTests.swift
 //  pizzaBotTests
 //
-//  Created by G Gajduk on 01/03/2019.
+//  Created by slice on 01/03/2019.
 //  Copyright © 2019 slice. All rights reserved.
 //
 
